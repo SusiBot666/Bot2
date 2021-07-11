20770d41038221be4f8029ad6556888b6db5e2c7
Susana <81292387+Susi666@users.noreply.github.com>
2021-07-11 10:53:16 -0600

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -6,19 +6,19 @@ handler.before = m => {
 	if (!user.autolevelup) return !0
 	let before = user.level * 1
 	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-        let role = ((user.level >= 1) && (user.level <= 10)) ? 'Beginner'
-          : ((user.level >= 10) && (user.level <= 20)) ? 'Kittel Town'
-          : ((user.level >= 20) && (user.level <= 30)) ? 'Black Woods'
-          : ((user.level >= 30) && (user.level <= 40)) ? 'Farmount'
-          : ((user.level >= 40) && (user.level <= 50)) ? 'Rosenvale'
-          : ((user.level >= 50) && (user.level <= 60)) ? 'Amberhill'
-          : ((user.level >= 60) && (user.level <= 70)) ? 'Master Fap'
-          : ((user.level >= 70) && (user.level <= 80)) ? 'Master Woods'
-          : 'Legend'
+        let role = ((user.level >= 1) && (user.level <= 10)) ? 'Principiante'
+          : ((user.level >= 10) && (user.level <= 20)) ? 'Aprendiz en proceso'
+          : ((user.level >= 20) && (user.level <= 30)) ? 'Noob'
+          : ((user.level >= 30) && (user.level <= 40)) ? 'Adicto al Bot'
+          : ((user.level >= 40) && (user.level <= 50)) ? 'Graduado'
+          : ((user.level >= 50) && (user.level <= 60)) ? 'Experto'
+          : ((user.level >= 60) && (user.level <= 70)) ? 'Maestro'
+          : ((user.level >= 70) && (user.level <= 80)) ? 'Gran Maestro'
+          : 'Leyenda'
 
 	if (before !== user.level) {
             m.reply(`
-Selamat, anda telah naik level!
+Felicitaciones, has subido de nivel!
 *${before}* -> *${user.level}*
 gunakan *.profile* untuk mengecek
 	`.trim())
