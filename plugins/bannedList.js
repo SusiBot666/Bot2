ace88a2922a2799bb6e615b4182d0b773dcf5cb0
Susana <81292387+Susi666@users.noreply.github.com>
2021-07-11 15:22:57 -0600

    Update bannedList.js

--- plugins/bannedList.js
+++ plugins/bannedList.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, usedPrefix }) => {
     let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
     
     m.reply(`
-┌ *Daftar Chat Terbanned*
+┌ *Lista de chats prohibidos*
 │ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
 │ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 │ ${jid}
 `.trim()).join('\n') : ''}
 └────
 
-┌ *Daftar User Terbanned*
+┌ *Lista de usuarios prohibidos*
 │ Total : ${users.length} User${users ? '\n' + users.map(([jid], i) => `
 │ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 │ ${jid}
