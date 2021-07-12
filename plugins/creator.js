76711d92932c55b85c2a96ba99836f69369e5d01
Susana <81292387+Susi666@users.noreply.github.com>
2021-07-11 22:24:21 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,9 +1,9 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+  this.sendContact(m.chat, '50377111111', 'Susi.li', m)
 }
-handler.help = ['owner', 'creator']
+handler.help = ['owner ᴺᵘᵐᵉʳᵒ ᵈᵉ ˡᵃ ᶜʳᵉᵃᵈᵒʳᵃ ᵈᵉˡ ᵇᵒᵗ', 'creator ᴺᵘᵐᵉʳᵒ ᵈᵉ ˡᵃ ᶜʳᵉᵃᵈᵒʳᵃ ᵈᵉˡ ᵇᵒᵗ']
 handler.tags = ['info']
 
 handler.command = /^(owner|creator)$/i
