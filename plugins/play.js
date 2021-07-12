6fc31c44a89152e298f46fca1a0f6c9293ca5ee0
Susana <81292387+Susi666@users.noreply.github.com>
2021-07-11 22:23:02 -0600

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -3,7 +3,7 @@ let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'Cari apa?'
+  if (!text) throw 'Que estas buscando ?'
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
@@ -18,17 +18,17 @@ let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
       usedServer = server
       break
     } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nprueba con otro servidor ...'}`)
     }
   }
   if (yt === false) throw 'Semua server tidak bisa :/'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*🔥Title:* ${title}
+*📂Filesize:* ${filesizeF}
+*✅Source:* ${vid.url}
+*${isLimit ? ' ': ''}💠Link:* ${dl_link}
 *Server y2mate:* ${usedServer}
 `.trim(), m)
 let _thumb = {}
@@ -44,7 +44,7 @@ if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(comm
   asDocument: chat.useDocument
 })
 }
-handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+handler.help = ['play', 'play2'].map(v => v + ' ᴱˢᶜʳᶦᵇᵃ ˡᵃ ᶜᵃⁿᶜᶦᵒ́ⁿ')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
 
