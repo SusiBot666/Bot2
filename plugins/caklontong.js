222e2c2d75a52c5a0155d4dd54a940474e6f5bfd
Susana <81292387+Susi666@users.noreply.github.com>
2021-07-11 21:22:24 -0600

    Update caklontong.js

--- plugins/caklontong.js
+++ plugins/caklontong.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
     if (id in conn.caklontong) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
+        conn.reply(m.chat, 'Todavía hay preguntas sin respuesta en este chat.', conn.caklontong[id][0])
         throw false
     }
     let res = await fetch(global.API('zahir', '/api/kuis/caklontong', {}, 'apikey'))
@@ -16,14 +16,14 @@ let handler = async (m, { conn, usedPrefix }) => {
 ${json.result.soal}
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}calo untuk bantuan
+Tipo ${usedPrefix} en busca de ayuda
 Bonus: ${poin} XP
 `.trim()
     conn.caklontong[id] = [
         await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*\n${json.result.deskripsi}`, conn.caklontong[id][0])
+            if (conn.caklontong[id]) conn.reply(m.chat, `Se acabó el tiempo!\nLa respuesta es *${json.result.jawaban}*\n${json.result.deskripsi}`, conn.caklontong[id][0])
             delete conn.caklontong[id]
         }, timeout)
     ]
