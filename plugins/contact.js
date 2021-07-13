d6fecdb17f038d9681bebafa199d78aa6aecc548
SusiBot666 <87385169+SusiBot666@users.noreply.github.com>
2021-07-13 15:39:44 -0600

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, text }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag salah satu lah'
+  if (!who) throw 'Etiqueta uno'
   txt = text.replace('@' + who.split`@`[0], '').trimStart()
   return conn.sendContact(m.chat, who, txt || conn.getName(who), m)
 }
 handler.help = ['save'].map(v => v + ' @mention <ContactName>')
-handler.tags = ['']
+handler.tags = ['exp']
 
 handler.command = /^save$/
 
