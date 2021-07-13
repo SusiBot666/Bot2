7a08d597efc57eb5631af2a3c531468c89c37d4d
Susana <81292387+Susi666@users.noreply.github.com>
2021-07-12 21:50:30 -0600

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -2,27 +2,37 @@ const { createHash } = require('crypto')
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.db.data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Format salah\n*${usedPrefix}daftar nama.umur*`
+  if (user.registered === true) throw `Ya te as registrado\nQuiere volver a registrarte? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Formato incorrecto\n*${usedPrefix}lista nombre.edad*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'Hey como te llamas, el nombre no puede estar estar vacío (Alphanumeric)'
+  if (!age) throw 'Hey Cuantos años tienes🥱'
   age = parseInt(age)
-  if (age > 120) throw 'Umur terlalu tua 😂'
-  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
+  if (age > 120) throw 'Amigo deja el wssp, y métete al asilo 😂'
+  if (age < 5) throw 'Los bebés no deben tener ni celular'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-Daftar berhasil!
-
-╭─「 Info 」
-│ Nama: ${name}
-│ Umur: ${age} tahun
-│ SN: ${sn}
-╰────
+「 REGISTRO EXITOSO 」
+⏥⏥⏥⏥⏥⏥⏥⏥⏥⏥
+⏜⏜⏜⏜⏜⏜⏜⏜
+⌲  *Informacion* 
+⏝⏝⏝⏝⏝⏝⏝⏝
+⏥⏥⏥⏥⏥⏥⏥⏥⏥⏥
+------------------------------
+⌬ \`\`\`Nombre: ${name}\`\`\`
+⌬ \`\`\`Edad: ${age} años\`\`\`
+⌬ \`\`\`SN: ${sn}\`\`\`
+------------------------------
+⏥⏥⏥⏥⏥⏥⏥⏥⏥⏥
+⏜⏜⏜⏜⏜⏜⏜⏜
+⍚ *NOTA*
+⚠NO VAYAS A PERDER TU *SN* POR QUE ES MUY IMPORTANTE:D
+⏝⏝⏝⏝⏝⏝⏝⏝
+⏥⏥⏥⏥⏥⏥⏥⏥⏥⏥
 `.trim())
 }
 handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
