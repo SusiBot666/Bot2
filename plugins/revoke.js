d872ba4a4196d8ea275ca0022b0faacd69aebc6c
SusiBot666 <87385169+SusiBot666@users.noreply.github.com>
2021-07-13 14:05:59 -0600

    Update revoke.js

--- plugins/revoke.js
+++ plugins/revoke.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, args, usedPrefix }) => {
   let res = await conn.revokeInvite(m.chat)
   m.reply('Link Group Berhasil Direset!\n\nLink Baru:\nhttps://chat.whatsapp.com/' + res.code)
 }
-handler.help = ['revoke']
+handler.help = ['revoke ᴿᵉᵉˢᵗᵃᵇˡᵉᶜᵉ ᵉˡ ᵉⁿˡᵃᶜᵉ ᵈᵉˡ ᵍʳᵘᵖᵒ']
 handler.tags = ['group']
 handler.command = /^re(voke|new)(invite|link)?$/i
 handler.group = true
