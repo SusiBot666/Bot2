55b2bc535090175c0907a88109425a021ff62191
SusiBot666 <87385169+SusiBot666@users.noreply.github.com>
2021-07-13 12:45:47 -0600

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -7,9 +7,9 @@ let handler = async (m, { isPrems }) => {
   m.reply(`+${isPrems ? prem : free} XP`)
   global.db.data.users[m.sender].lastclaim = new Date * 1
 }
-handler.help = ['daily', 'claim']
+handler.help = ['reclamar']
 handler.tags = ['xp']
-handler.command = /^(daily|claim)$/i
+handler.command = /^(reclamar)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
