d5ee962e78d71605f48e063a582e3300985d4ff2
SusiBot666 <87385169+SusiBot666@users.noreply.github.com>
2021-07-13 15:34:35 -0600

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -1,15 +1,15 @@
 // By RC047 :V
 
 let handler = async(m, { conn, text }) => {
-    if (!text) throw 'Silahkan masukkan laporan'
-    if (text.length > 300) throw 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks!'
-    const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
+    if (!text) throw 'Ingrese un informe'
+    if (text.length > 300) throw 'Lo siento, el texto es demasiado largo, máximo 300 textos!'
+    const laporan = `*「 REPORT 」*\nNumero : wa.me/${m.sender.split`@`[0]}\nQueja : ${text}`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6281515860089@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) // Mwehehehehe
-    m.reply('✔️Masalah telah di laporkan ke Owner Bot, laporan palsu/main2 tidak akan ditanggapi!')
+    m.reply('️El problema se ha informado al propietario del robot, no se responderán los informes falsos!')
 }
-handler.help = ['bug', 'report'].map(v => v + ' <laporan>')
+handler.help = ['bug',].map(v => v + ' ᴿᵉᵖᵒʳᵗᵃ ᶜᵘᵃˡᑫᵘᶦᵉʳ ᵇᵘᵍ ʳᵉᵃˡ')
 handler.tags = ['info']
 handler.command = /^(bug|report)$/i
 
