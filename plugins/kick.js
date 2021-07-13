9c9435e6100b710bf676649575bb6edc6c7987f0
SusiBot666 <87385169+SusiBot666@users.noreply.github.com>
2021-07-13 13:59:40 -0600

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
-handler.help = ['kick', '-'].map(v => v + ' @user')
+handler.help = ['eliminar'].map(v => v + ' @ᵁˢᵘᵃʳᶦᵒ')
 handler.tags = ['admin']
-handler.command = /^(kick|\-)$/i
+handler.command = /^(eliminar)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
