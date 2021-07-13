bea2d3accb9409076a7be64003174e8c98d42c40
SusiBot666 <87385169+SusiBot666@users.noreply.github.com>
2021-07-13 13:40:42 -0600

    Update pay.js

--- plugins/pay.js
+++ plugins/pay.js
@@ -1,20 +1,20 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.02
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Masukkan jumlah exp yang akan diberi'
+  if (!text) throw 'Ingrese la cantidad de exp que se dará'
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag salah satu lah'
+  if (!who) throw 'Etiqueta uno'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (isNaN(txt)) throw 'Hanya angka'
+  if (isNaN(txt)) throw 'sólo números'
   let xp = parseInt(txt)
   let exp = xp
   let pjk = Math.ceil(xp * pajak)
   exp += pjk
-  if (exp < 1) throw 'Minimal 1'
+  if (exp < 1) throw 'Mínimo 1'
   let users = global.db.data.users
-  if (exp > users[m.sender].exp) throw 'Exp tidak mencukupi untuk mentransfer'
+  if (exp > users[m.sender].exp) throw 'Experiencia insuficiente para transferir'
   users[m.sender].exp -= exp
   users[who].exp += xp
 
