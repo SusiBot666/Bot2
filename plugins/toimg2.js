252364eb71cb2fefbc50cfa0184a423b6338fe16
SusiBot666 <87385169+SusiBot666@users.noreply.github.com>
2021-07-13 13:53:29 -0600

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     thumbnail: Buffer.alloc(0)
   })
 }
-handler.help = ['toimg2 (reply)']
+handler.help = ['toimg ᶜʳᵉᵃ ᵉˡ ˢᵗᶦᶜᵏᵉʳ ᵉⁿ ᶦᵐᵃᵍᵉⁿ']
 handler.tags = ['sticker']
-handler.command = ['toimg2']
+handler.command = ['toimg']
 module.exports = handler
